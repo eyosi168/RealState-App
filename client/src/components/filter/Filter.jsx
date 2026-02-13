5efaffecadf32b100f1f07398605b70c5a24def1
eyosi168 <eyosisol123@gmail.com>
2026-02-13 17:25:32 +0300

    workng on the components

---
+++ client/src/components/filter/Filter.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import "./filter.scss"
+function Filter() {
+  return (
+    <div className="filter">
+      <h1>
+        Search result for <b>ADDIS ABABA</b>
+      </h1>
+      <div className="top">
+        <div className="item">
+          <label htmlFor="city"> Location</label>
+          <input
+            type="text"
+            name="city"
+            id="city"
+            placeholder="City Location"
+          />
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="item">
+          <label htmlFor="city"> Type</label>
+          <select name="type" id="type">
+          <option value="">Any</option>
+            <option value="buy">Buy</option>
+            <option value="rent">Rent</option>
+
+          </select>
+        </div>
+        <div className="item">
+          <label htmlFor="property"> Property</label>
+         <select name="property" id="property">
+            <option value="">Any</option>
+            <option value="apartment">Apartment</option>
+            <option value="house">House</option>
+            <option value="condo">Condo</option>
+            <option value="land">Land</option>
+         </select>
+        </div>
+        <div className="item">
+          <label htmlFor="minPrice"> Min Price</label>
+          <input
+            type="number"
+            name="minPrice"
+            id="minPricie"
+            placeholder="any"
+          />
+        </div>
+        <div className="item">
+          <label htmlFor="maxPrice"> Max price</label>
+          <input
+            type="number"
+            name="maxPrice"
+            id="maxPrice"
+            placeholder="any"
+          />
+        </div>
+        <div className="item">
+          <label htmlFor="bedroom"> Bedroom</label>
+          <input
+            type="number"
+            name="bedroom"
+            id="bedroom"
+            placeholder="any"
+          />
+        </div>
+        <button>
+          <img src="/search.png" alt="" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Filter;
--- client/src/components/routes/listPage/ListPage.jsx
+++ client/src/components/routes/listPage/ListPage.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import "./listPage.scss";
+import { listData } from '../../../lib/dummydata'
+import Filter from '../../filter/Filter';
+import Card from '../../card/Card';
 
 function ListPage() {
+    const data = listData;
   return (
-    <div>
-      list pagr
+    <div className='listPage'>
+      <div className="listContainer">
+        <div className="wrapper">
+            <Filter/>
+            {data.map(item=>(
+                <Card key={item.id} item={item}/>
+            ))}
+        </div>
+        </div>
+      <div className="mapContainer"></div>
     </div>
   )
 }
