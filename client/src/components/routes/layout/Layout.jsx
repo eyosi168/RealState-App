1f850e6ab7162f98590d18917643218a49c4e493
eyosi168 <eyosisol123@gmail.com>
2026-02-15 22:45:31 +0300

    parent jsx

--- client/src/components/routes/layout/Layout.jsx
+++ client/src/components/routes/layout/Layout.jsx
@@ -11,7 +11,7 @@ function Layout() {
           <Navbar></Navbar>
         </div>
         <div className="content">
-           
+           {/* to the parent layout */}
           <Outlet/>
         </div>
       </div>
